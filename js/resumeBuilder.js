5508cb486008517590955a0ef17e922cc87406dd
Nick <phix@users.noreply.github.com>
2016-03-16 12:27:01 -0500

    corrected missing json elements

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,9 +2,9 @@ var bio = {
 	"name": "Nicholas Ratliff",
 	"role": "Front End Application Developer",
 	"contacts": {"mobile": "3145205145", "email": "1337.geek@gmail.com", "github": "phix.github.io", "twitter": "@nicholasratliff", "location": "St Louis Missouri"},
-	"welcomeMessage": "Welcome!",
-        "skills": ["VBA", "Classic ASP", "VBScript", "Batch", "C#", "Coldfusion", "Java", "IIS", "Websphere"],
-        "biopic": './images/my_picture.jpg'
+    "welcomeMessage": "Welcome!",
+    "skills": ["VBA", "Classic ASP", "VBScript", "Batch", "C#", "Coldfusion", "Java", "IIS", "Websphere"],
+    "biopic": './images/my_picture.jpg'
 };
 
 bio.display = function(){
@@ -37,20 +37,73 @@ bio.display = function(){
 
 bio.display();
 
+var education = {
+	"schools": [{
+			"name": "Fort Zumwalt North Highschool",
+			"location": "Ofallon Missouri",
+			"degree": "NA",
+			"majors": "Programming",
+			"dates": "1996-2000",
+			"url" : "www.nowhere.com"
+		}, {
+			"name": "Example School",
+			"location": "St Charles Missouri",
+			"degree": "NA",
+			"majors": "NA",
+			"dates": "2000-2004",
+			"url": "www.nowhere.com"
+		}
+
+
+	],
+	"onlineCourses": [{
+		"title": "NA",
+		"school": "NA",
+		"date": "NA",
+		"url": "NA"
+	}]
+}
+
+education.display = function(){
+
+	if(education.schools.length > 0){		
+		$("#education").append(HTMLschoolStart)
+		for(x in education.schools) {
+			var eName = HTMLschoolName.replace("%data%",education.schools[x].name);
+			var eLoc = HTMLschoolLocation.replace("%data%",education.schools[x].location);
+			var eDegree = HTMLschoolDegree.replace("%data%",education.schools[x].degree);
+			var eMajor = HTMLschoolMajor.replace("%data%",education.schools[x].majors);
+			var eDates = HTMLschoolDates.replace("%data%",education.schools[x].dates);				
+			$(".education-entry:last").append(eName).append(eLoc).append(eDates).append(eMajor);		 	
+		};
+	}
+
+	if(education.onlineCourses.length > 0){
+		for(x in education.onlineCourses) {
+			var oTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[x].title);
+			var oSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[x].school);
+			var oDate = HTMLonlineDates.replace("%data%",education.onlineCourses[x].date);
+			var oUrl = HTMLonlineURL.replace("%data%",education.onlineCourses[x].url);
+			$(".education-entry:last").append(HTMLonlineClasses).append(oTitle + " - " + oSchool).append(oDate).append(oUrl)	
+		};
+	}	 
+};
+education.display();
+
 var work = {
 	"jobs": [
 		{
 			"employer": "AT&T",
 			"title": "Sr Specialist Application Developer",
 			"location": "Earth City, Missouri",
-			"datesWorked": "Nov 2013 - Current",
+			"dates": "2015 - Current",
 			"description": "Application Developer for ATO and CARE Organizations"
 		},
 		{
 			"employer": "AT&T",
-			"title": "Sr Specialist Application Developer",
+			"title": "SR It Analyst",
 			"location": "Earth City, Missouri",
-			"datesWorked": "Nov 2013 - Current",
+			"dates": "Nov 2013 - 2015",
 			"description": "Application Developer for ATO and CARE Organizations"
 		}]
 };
@@ -60,7 +113,7 @@ work.display = function(){
 	var wEmployer = HTMLworkEmployer.replace("%data%", work.jobs[0].employer);
 	var wWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[0].title);
 	var wWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[0].location);
-	var wDatesWorked = HTMLworkDates.replace("%data%", work.jobs[0].datesWorked);
+	var wDatesWorked = HTMLworkDates.replace("%data%", work.jobs[0].dates);
 	var wWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[0].description);
 
 	$("#workExperience").append(HTMLworkStart);
@@ -88,57 +141,7 @@ projects.display = function(){
 
 projects.display();
 
-var education = {
-	"schools": [{
-			"name": "Fort Zumwalt North Highschool",
-			"location": "Ofallon Missouri",
-			"degree": "NA",
-			"majors": "Programming",
-			"dates": "1996-2000"
-		}, {
-			"name": "Example School",
-			"location": "St Charles Missouri",
-			"degree": "NA",
-			"majors": "NA",
-			"dates": "2000-2004"
-		}
 
-
-	],
-	"onlineCourses": [{
-		"title": "NA",
-		"school": "NA",
-		"date": "NA",
-		"url": "NA"
-
-	}]
-}
-
-education.display = function(){
-
-	if(education.schools.length > 0){		
-		$("#education").append(HTMLschoolStart)
-		for(x in education.schools) {
-			var eName = HTMLschoolName.replace("%data%",education.schools[x].name);
-			var eLoc = HTMLschoolLocation.replace("%data%",education.schools[x].location);
-			var eDegree = HTMLschoolDegree.replace("%data%",education.schools[x].degree);
-			var eMajor = HTMLschoolMajor.replace("%data%",education.schools[x].majors);
-			var eDates = HTMLschoolDates.replace("%data%",education.schools[x].dates);	
-			$(".education-entry:last").append(eName).append(eLoc).append(eDates).append(eMajor);		 	
-		};
-	}
-
-	if(education.onlineCourses.length > 0){
-		for(x in education.onlineCourses) {
-			var oTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[x].title);
-			var oSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[x].school);
-			var oDate = HTMLonlineDates.replace("%data%",education.onlineCourses[x].date);
-			var oUrl = HTMLonlineURL.replace("%data%",education.onlineCourses[x].url);
-			$(".education-entry:last").append(HTMLonlineClasses).append(oTitle + " - " + oSchool).append(oDate).append(oUrl)	
-		};
-	}	 
-};
-education.display();
 
 $("#mapDiv").append(googleMap);
 
